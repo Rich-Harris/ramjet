fd50689c925c579a64a439f56dcbbc1d55dcc903
Rich Harris <richard.a.harris@gmail.com>
2015-04-22 17:44:27 -0400

    tidy up repo

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,7 @@ PATH=node_modules/.bin:$PATH
 
 gobble build -f .tmp
 
-rm dist/*
-cp .tmp/ramjet* dist
+rm -rf dist
+mkdir -p dist
+cp .tmp/ramjet.js dist/ramjet.js
+cp .tmp/ramjet.min.js dist/ramjet.min.js
