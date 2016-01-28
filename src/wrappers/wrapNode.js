b0b3f4b4514669017e33e91f1425eb46bf898b26
Rich Harris <richard.a.harris@gmail.com>
2016-01-28 10:19:22 -0500

    remove unused code

--- src/wrappers/wrapNode.js
+++ src/wrappers/wrapNode.js
@@ -1,9 +1,5 @@
 import HtmlWrapper from './HtmlWrapper';
-import { svgns } from '../utils/svg';
 
 export default function wrapNode ( node, options ) {
-	return node.namespaceURI === svgns ?
-		//new SvgWrapper( node, options ) :
-		new HtmlWrapper( node, options ) :
-		new HtmlWrapper( node, options );
+	return new HtmlWrapper( node, options );
 }
